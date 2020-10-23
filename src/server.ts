8e838c0969067a44bbf42603ebed83bf454162e5
Predrag Stojadinovic <predrag@stojadinovic.net>
2020-10-23 16:24:55 +0200

    refactoring

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,35 @@
 #!/usr/bin/env node
 "use strict";
 
-import {AddressInfo} from "net";
-import {Server, IncomingMessage, ServerResponse} from "http"
 import * as path from "path";
 import * as dotenv from "dotenv";
+
+import {AddressInfo} from "net";
+import {Server, IncomingMessage, ServerResponse} from "http";
 
 dotenv.config({path: ".env.local"});
 // IMPORTANT: must execute dotenv before importing anything
 // that depends on process.env (like MongoDBConnector, for example)
 
-import {fastify, FastifyInstance} from "fastify";
+import {ContextConfigDefault, fastify, FastifyInstance, FastifyReply, FastifyRequest} from "fastify";
+import {RouteGenericInterface} from "fastify/types/route";
+
+import * as fastifyJwt from "fastify-jwt";
+import * as fastifyWebsocket from "fastify-websocket";
 import fastifyCors from "fastify-cors";
 import fastifyBlipp from "fastify-blipp";
 import fastifyHelmet from "fastify-helmet";
-import * as fastifyJwt from "fastify-jwt";
-import * as fastifyWebsocket from "fastify-websocket";
 
 import websocket from "./_reactivestack/_f.websocket";
 import addRoutes from "./_reactivestack/util/_f.add.routes";
-import MongoDBConnector from "./_reactivestack/mongodb.connector";
 import processModels from "./_reactivestack/_f.process.models";
+import MongoDBConnector from "./_reactivestack/mongodb.connector";
 
-// const fastify = Fastify({logger: false});
 const server: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({logger: false})
 
+server.register(fastifyBlipp);
+server.register(fastifyHelmet);
+server.register(fastifyWebsocket);
 server.register(fastifyJwt, {secret: process.env.JWT_SECRET});
 server.register(fastifyCors, {
 	// put your options here
@@ -35,36 +40,35 @@ server.register(fastifyCors, {
 		"http://localhost:3006"
 	]
 });
-server.register(fastifyBlipp);
-server.register(fastifyHelmet);
-server.register(fastifyWebsocket);
 
-const addJWTHook = (srv) => {
-	srv.addHook("onRequest", async (request, reply) => {
+const _addJWTHook = (srv: FastifyInstance<Server, IncomingMessage, ServerResponse>): void => {
+	srv.addHook("onRequest", async (request: FastifyRequest<RouteGenericInterface, Server, IncomingMessage>, reply: FastifyReply<Server, IncomingMessage, ServerResponse, RouteGenericInterface, ContextConfigDefault>) => {
 		try {
 			await request.jwtVerify();
 		} catch (err) {
-			// reply.send(err);
+			reply.send(err);
 		}
 	});
 };
 
-const addWebSocketListener = (srv) => srv.get("/ws", {websocket: true}, websocket);
+const _addWebSocketListener = (srv: FastifyInstance<Server, IncomingMessage, ServerResponse>): void => {
+	srv.get("/ws", {websocket: true}, websocket);
+};
 
 // Run the server!
 const startFastifyServer = async () => {
 	try {
 		processModels(path.join(__dirname, "models"));
-
 		MongoDBConnector.init();
 
-		addJWTHook(server);
+		_addJWTHook(server);
 
-		addWebSocketListener(server);
+		_addWebSocketListener(server);
 
 		addRoutes(server, path.join(__dirname, "routes"));
 
-		await server.listen(parseInt(process.env.PORT, 10));
+		await server.listen(parseInt(process.env.PORT || "3003", 10));
+		console.log("");
 		server.blipp();
 		server.log.info(`Server listening on port ${(server.server.address() as AddressInfo).port}.`);
 
@@ -78,5 +82,5 @@ startFastifyServer()
 	.then(() => ({}))
 	.catch((err) => console.error("Server Error:", err));
 
-process.on("uncaughtException", (reason, p) => console.error("Uncaught Exception at:", p, "reason:", reason));
-process.on("unhandledRejection", (reason, p) => console.error("Unhandled Rejection at:", p, "reason:", reason));
+process.on("uncaughtException", (reason: string, p: Promise<any>): void => console.error("Uncaught Exception at:", p, "reason:", reason));
+process.on("unhandledRejection", (reason: string, p: Promise<any>): void => console.error("Unhandled Rejection at:", p, "reason:", reason));
