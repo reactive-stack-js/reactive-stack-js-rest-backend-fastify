d6b2fa52753f9a93f9af9c061e389b476b721e58
predrags <predrags@nvidia.com>
2021-02-18 17:55:57 +0100

    refactoring

--- src/_auth/user.manager.ts
+++ src/_auth/user.manager.ts
@@ -1,34 +1,66 @@
 #!/usr/bin/env node
 'use strict';
+
+import {get} from 'lodash';
+import * as jsonwebtoken from 'jsonwebtoken';
+
 import jwtTokenRefresh from './_f.jwt.token.refresh';
 import IUserManager, {UserManagerRefreshType} from './_i.user.manager';
 
 export default class UserManager implements IUserManager {
+	private _location: string;
 	private _jwt: string;
+	private _user: any;
 	private readonly _jwtSecret: string;
 
 	public constructor(jwtSecret: string) {
 		this._jwtSecret = jwtSecret;
+		this._location = '';
 	}
 
-	public connected(jwt: string): void {
+	public connected(jwt: any): void {
 		this._jwt = jwt;
 
-		// TODO: verify user exists, and so on...
-		// const user = jsonwebtoken.verify(jwt, this._jwtSecret);
-		// TODO: update database
+		const user = jsonwebtoken.verify(jwt, this._jwtSecret);
+		if (user) {
+			this._user = user;
+			const userId = get(this._user, '_id');
+			if (userId) {
+				// TODO: update database
+				// await User.updateOne({_id: userId}, {$set: {online: true}});
+				// if (this._location) await User.updateOne({_id: userId}, {$set: {location: this._location}});
+			}
+		}
 	}
 
 	ping(ping: number): void {
-		// TODO: update database
+		const userId = get(this._user, '_id');
+		if (userId) {
+			// TODO: update database
+			// await User.updateOne({_id: userId}, {$set: {ping}});
+		}
 	}
 
 	location(location: string): void {
-		// TODO: update database
+		if (location === 'odjava') return this.disconnected();
+		this._location = location;
+		const userId = get(this._user, '_id');
+		if (userId) {
+			// TODO: update database
+			// await User.updateOne({_id: userId}, {$set: {location: this._location}});
+		}
 	}
 
 	disconnected(): void {
-		// TODO: update database
+		const userId = get(this._user, '_id');
+		if (userId) {
+			// TODO: update database
+			// await User.updateOne({_id: userId}, {$set: {online: false}});
+			// await User.updateOne({_id: userId}, {$unset: {location: ''}});
+			// await User.updateOne({_id: userId}, {$unset: {table: ''}});
+		}
+		this._location = '';
+		this._user = null;
 	}
 
 	public checkSession(): UserManagerRefreshType {
