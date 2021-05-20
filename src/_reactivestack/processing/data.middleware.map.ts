353f185eaf352f1a340c96e695802b3a67b1ded5
Predrag Stojadinovic <predrag@stojadinovic.net>
2021-05-20 22:27:56 +0200

    refactoring

--- src/_reactivestack/processing/data.middleware.map.ts
+++ src/_reactivestack/processing/data.middleware.map.ts
@@ -3,17 +3,17 @@
 
 const criteria = (scope: string, collection: string): string => `${scope}-${collection}`;
 
-export default class DataProcessorsMap {
+export default class DataMiddlewareMap {
 	public static addProcessor(scope: string, collection: string, processor: Function): void {
-		DataProcessorsMap._processors.set(criteria(scope, collection), processor);
+		DataMiddlewareMap._processors.set(criteria(scope, collection), processor);
 	}
 
 	public static hasProcessor(scope: string, collection: string): boolean {
-		return !!DataProcessorsMap._processors.get(criteria(scope, collection));
+		return !!DataMiddlewareMap._processors.get(criteria(scope, collection));
 	}
 
 	public static getProcessor(scope: string, collection: string): Function | null {
-		return DataProcessorsMap._processors.get(criteria(scope, collection));
+		return DataMiddlewareMap._processors.get(criteria(scope, collection));
 	}
 
 	private static readonly _processors: Map<string, Function> = new Map<string, Function>();
